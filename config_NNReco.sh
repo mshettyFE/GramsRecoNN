8c2bfd2f8b973654f4dbd95fc1e9c788672e9aa1
Mihir Shetty <ms6556@columbia.edu>
2023-11-28 20:32:51 -0500

    Got GramsSimSensitivity submodule

--- config_NNReco.sh
+++ config_NNReco.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 git submodule init
 git submodule update
-rm -rf GenData/GramsWork
-mkdir GenData/GramsWork
-cd GenData/GramsWork
+rm -rf GenData
+mkdir GenData
+cd GramsSimSensitivity
+./config.sh
+cd ../GenData
 if [ $? -ne 0 ]
 then
   exit
 fi
-cmake ../../GramsSim
+cmake ../GramsSimSensitivity
 make
-cd ../../SparseConvNet
+cd ../SparseConvNet
 export TORCH_CUDA_ARCH_LIST="6.0;6.1;6.2;7.0;7.5"
 rm -rf build/ dist/ sparseconvnet.egg-info sparseconvnet_SCN*.so
 python3 setup.py develop
