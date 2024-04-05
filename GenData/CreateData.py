b9fe5d3879b6fbbbdd7c73e8c7363932896ae345
mshettyFE <ms6556@columbia.edu>
2024-04-05 15:39:41 -0400

    Fleshed out GramsDetSim/GramsReadoutSim Tensor generation

--- GenData/CreateData.py
+++ GenData/CreateData.py
@@ -20,6 +20,7 @@ import argparse
 
 import numpy as np
 import sys,os, random,time
+from enum import Enum
 
 # Add the directory above MCTruth to Python path. Needed to access TomlSanityCheck, which does data  validation on Config file
 # TomlSanityCheck.py is in the parent directory since I might want to use those commands in another directory in the repo
@@ -29,6 +30,10 @@ from  TomlSanityCheck import TomlSanityCheck
 # sorted Branches of Gramsg4 root tuple, along with map
 TrackInfo_keys = ['Etot', 'Event', 'PDGCode', 'ParentID', 'ProcessName', 'Run', 'TrackID', 'identifier', 'px', 'py', 'pz', 't', 'x', 'y', 'z']
 TrackInfo_keys_map = {k:v for k,v in zip(TrackInfo_keys,range(len(TrackInfo_keys)))}
+# same thing, but with ReadoutSim
+Readout_keys = ["Run", "Event", "TrackID", "PDGCode", "numPhotons", "cerPhotons", "energy", "numElectrons", "x","y","z","timeAtAnode","num_step", "identifier","pixel_idx","pixel_idy"]
+Readout_keys.sort()
+Readout_keys_map = {k:v for k,v in zip(Readout_keys,range(len(Readout_keys)))}
 
 class Position:
 # Very bare bones R^3 vector. Supports addition, subtraction, L2 norm and dot product, which are the only operations I can about for Scatter Series Reconstruction
@@ -80,10 +85,40 @@ class GramsG4Entry:
     def emit_data(self):
         return (self.position, self.time, self.energy)
 
+class GramsReadoutEntry:
+# Encapsulates a single entry in the ReadoutSim branch of Readout
+# effectively just a C struct
+    def __init__(self,dataframe: np.ndarray):
+        if (dataframe.shape[0] != 16):
+            raise Exception("This hit cannot be reconstructed")
+        self.position = Position(dataframe[ReadoutIndex('x')],dataframe[ReadoutIndex('y')],dataframe[ReadoutIndex('z')])
+        self.time = dataframe[ReadoutIndex('timeAtAnode')]
+        self.energy = dataframe[ReadoutIndex('energy')]*dataframe[ReadoutIndex('numElectrons')] # Multiple energy (per cluster) by the number of electron clusters to get total energy
+        self.identifier = dataframe[ReadoutIndex('identifier')]
+        self.pixel_indices = (int(dataframe[ReadoutIndex('pixel_idx')]),int(dataframe[ReadoutIndex('pixel_idy')]))
+
+    def __repr__(self):
+        output = ""
+        output += "\tPosition:"+self.position.__str__()
+        output +="\t"+"Time: " +  str(self.time)+"\n"
+        output +="\t"+"Energy: " +  str(self.energy)+"\n"
+        output +="\t"+"ID: " +  str(self.identifier)+"\n"
+        return output
+    def emit_positions(self):
+        return self.position
+    def emit_data(self):
+        return (self.position, self.time, self.energy)
+
+class SeriesType(Enum):
+    MCTruth =1
+    Readout = 2
+
 class ScatterSeries:
-# Stores a list of GramsG4Entries, and determines if the series is reconstructable with Compton Cone Method
-    def __init__(self):
+# Stores a list of Entries, and determines if the series is reconstructable with Compton Cone Method
+# series_type can be a part of the set: {"MC","Readout"}
+    def __init__(self,series_type: SeriesType):
         self.scatter_series = []
+        self.series_type = series_type
     def __repr__(self):
         count = 1
         output = ""
@@ -95,13 +130,15 @@ class ScatterSeries:
         return output
     def __len__(self):
         return len(self.scatter_series)
-    def add(self,scatter: GramsG4Entry):
+    def add(self,scatter):
     # Python lets you do type annotations now, which is neat, and useful in this case since we really only want GramsG4Entries here
         self.scatter_series.append(scatter)
     def sort(self):
     # Sort the scatter series by time
         self.scatter_series.sort(reverse=False, key= lambda scatter: scatter.time)
     def reconstructable(self):
+        if(self.series_type == SeriesType.Readout):
+            raise Exception("Can't determine reconstructability of Readout data")
         if(len(self.scatter_series) <3):
             return False
         valid_interactions = ["Primary", "phot", "compt"]
@@ -125,6 +162,8 @@ class ScatterSeries:
 
     def output_tuple(self):
     # Calculates the truth level energy and truth-level initial scattering angle (in radians)
+        if(self.series_type==SeriesType.Readout):
+            raise Exception("Can't calculate MCTruth values from Readoutsim tuple")
         if(len(self.scatter_series) < 3):
             raise Exception("Scatter Series must be of at least length 3")
         e_type = self.escape_type()
@@ -165,7 +204,7 @@ def pixellate(xval,yval, xDim, yDim, PixelCountX, PixelCountY):
             raise Exception("yval outside TPC. Maybe not filtering series correctly?")
     return (nx,ny)
 
-def CreateTensor(configuration, input_data, output_data, run):
+def CreateTensor(configuration, input_data, output_data,run):
 # This assumes TomlSanityChecker has vetted configuration file
 # Mass of electron in MeV. Need to subtract from each hit to get deposition energy
     mass_e =  0.51099895
@@ -173,13 +212,15 @@ def CreateTensor(configuration, input_data, output_data, run):
     if (Geo == "cube"):
         xDim = 70
         yDim = 70
-    if (Geo == "flat"):
+    elif (Geo == "flat"):
         xDim = 140
         yDim = 140
+    else:
+        raise Exception("Invalid Geometry")
     PixelCountX = int(configuration["GenData"]["PixelCountX"]["value"])
     PixelCountY = int(configuration["GenData"]["PixelCountY"]["value"])
 # 1 stands for number of channels (needed to be compatible with 2D conv layers)
-# For Final version, will need to increase number of channels
+# For Final version, will need to increase number of channels to take into account time
     input_labels = np.zeros((len(input_data),1,PixelCountX, PixelCountY) )
 # 1 and 3 stands for a row vector with energy, reconstruction angle, and escape type as columns
     output_labels = np.zeros((len(input_data), 1, 3))
@@ -213,11 +254,54 @@ def CreateTensor(configuration, input_data, output_data, run):
     }
     return tensors
 
+def CreateReadoutTensor(configuration, input_data, output_data, run):
+# This assumes TomlSanityChecker has vetted the configuration file
+# Differs from CreateTensor since we don't use pixellate() here
+    PixelCountX = int(configuration["GenData"]["PixelCountX"]["value"])
+    PixelCountY = int(configuration["GenData"]["PixelCountY"]["value"])
+# 1 stands for number of channels (needed to be compatible with 2D conv layers)
+# For Final version, will need to increase number of channels to take into account time
+    input_labels = np.zeros((len(input_data),1,PixelCountX, PixelCountY))
+# 1 and 3 stands for a row vector with energy, reconstruction angle, and escape type as columns
+    output_labels = np.zeros((len(input_data), 1, 3))
+    count = 0
+    for series_id in input_data:
+# Initialize empty anode plane with no depositions
+        anode_grid = np.zeros((PixelCountX,PixelCountY))
+# Grab the specific scatter series
+        LArHits = input_data[series_id].scatter_series
+        if(len(LArHits) ==0):
+            continue
+        for hit in LArHits:
+            anode_indices = hit.pixel_indices
+# Add energy of deposition to correct pixel
+            anode_grid[anode_indices[0], anode_indices[1]] += (hit.energy)
+# At this point, have a 2D "image" of energy depositions. We get associate output data, and place data in big numpy array
+        input_labels[count,0,:,:] = anode_grid
+        output_labels[count,:,:] = output_data[series_id]
+        count += 1
+    input_labels = from_numpy(input_labels)
+    output_labels = from_numpy(output_labels)
+    input_name = "input_anode_images"
+    output_name = "output_anode_images"
+    tensors = {
+        input_name: input_labels,
+        output_name: output_labels
+    }
+    return tensors
+
 def TrackInfoIndex(label:str):
     global TrackInfo_keys_map
     if label not in TrackInfo_keys_map:
         raise Exception("invalid gramsg4 label")
     out = TrackInfo_keys_map[label]
+    return out
+
+def ReadoutIndex(label:str):
+    global Readout_keys_map
+    if label not in Readout_keys_map:
+        raise Exception("invalid readoutsim label")
+    out = Readout_keys_map[label]
     return out
 
 
@@ -256,7 +340,7 @@ def ReadG4Root(configuration, gramsg4_path):
                 scatters = np.stack(scatters)
                 gamma = np.concatenate(gamma)
                 # Generate the scatter series for this event
-                cur_series = ScatterSeries()
+                cur_series = ScatterSeries(SeriesType.MCTruth)
                 cur_series.add(GramsG4Entry(gamma))
                 for hit in range(scatters.shape[1]):
                     cur_series.add(GramsG4Entry(scatters[:,hit]))
@@ -269,6 +353,52 @@ def ReadG4Root(configuration, gramsg4_path):
             output_tuple = output_mctruth_series[key].output_tuple()
             output_energy_angle[key] = output_tuple
     return output_mctruth_series, output_energy_angle
+
+def ReadReadoutSim(GramsConfig,readout_file,gramsg4_output_series):
+    readout_input_series = {}
+    # Get resolution of anode pixel grid
+    PixelCountX = int(GramsConfig["GenData"]["PixelCountX"]["value"])
+    PixelCountY = int(GramsConfig["GenData"]["PixelCountY"]["value"])
+    # Open up root file and get TrackInfo TTree
+    with uproot.open(readout_file+":ReadoutSim") as readout:
+        keys = readout.keys()
+        keys  = list(keys)
+        keys.sort()
+        data =  readout.arrays(library='np')
+        # Doing this since I couldn't find any syntax that allows you to index a numpy array with entries of variable length
+        # We only care about the first entry when there are multiple deposits in a hit
+        for i in range(data['timeAtAnode'].shape[0]):
+            data['timeAtAnode'][i] = data['timeAtAnode'][i][0]
+            data['x'][i] = data['x'][i][0]
+            data['y'][i] = data['y'][i][0]
+            data['z'][i] = data['z'][i][0]
+            # Grab Last entry in energy and numElectrons vectors (seems to be the right value, since the rest are fixed to some integer)
+            data['energy'][i] = data['energy'][i][-1]
+            data['numElectrons'][i] = data['numElectrons'][i][-1]
+            # Add an offset to pixel ID. GramsReadoutSim maps pixels from [-(PixelCountX/2), (PixelCountX/2)]
+            # Python indexing doesn't treat negatives properly with this map, so we right shift everything so that the lower bound is 0;
+            data['pixel_idx'][i] = data['pixel_idx'][i][0]+ np.floor(PixelCountX/2)
+            data['pixel_idy'][i] = data['pixel_idy'][i][0]+ np.floor(PixelCountY/2)
+        unique_event_mask = np.unique(data["Event"]) # Grab all the unique events
+        unique_run_mask = np.unique(data["Run"]) # Grab all unique runs
+        for run in unique_run_mask:
+            for event in unique_event_mask:
+                key = (run,event)
+# Check if there is a match in the gramsg4_output_series file. If not, then move on
+                if(key not in gramsg4_output_series):
+                    continue
+# We found a match! Get the data from readoutsim
+                event_mask = np.logical_and((data["Run"]==run), ( data["Event"]==event)) # Grab all hits coming from the same initial gamma ray
+                scatters = [data[str(key)][event_mask] for key in keys]
+                # package the data into a single numpy array
+                scatters = np.stack(scatters)
+                # Generate the scatter series for this event
+                cur_series = ScatterSeries(SeriesType.Readout)
+                for hit in range(scatters.shape[1]):
+                    hit  = GramsReadoutEntry(scatters[:,hit])
+                    cur_series.add(hit)
+                readout_input_series[(run,event)] = cur_series
+    return readout_input_series, gramsg4_output_series
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='CreateMCNNData')
@@ -303,18 +433,27 @@ if __name__ == "__main__":
     random.seed(time.time())
     output_tensor = {}
     meta = {}
+# Read MCTruth Data from GramsG4
+    gramsg4_file = os.path.join(hm,"GramsSimWork","gramsg4.root")
+    input_data, output_data = ReadG4Root(GramsConfig, gramsg4_file)
+    readout_file = os.path.join(hm,"GramsSimWork","gramsreadoutsim.root")
     if (GramsConfig["GenData"]["MCTruth"]["value"]):
-        # Run simulation, and generate tensors to pass to PyTorch
-        gramsg4_file = os.path.join(hm,"GramsSimWork","gramsg4.root")
-        input_data, output_data = ReadG4Root(GramsConfig, gramsg4_file)
         new = CreateTensor(GramsConfig, input_data, output_data,GramsConfig["GenData"]["RunID"]["value"])
-        output_tensor.update(new)
-    # Add to meta data how many images were generated in each run for a given batch
-        first = list(new.keys())[0]
-        meta[str(GramsConfig["GenData"]["RunID"]["value"])] = str(new[first].shape[0])
-        if (GramsConfig["GenData"]["Debug"]["value"]):
-            print(meta)
+# Generate data with detector effects
+    else:
+# Grab output from Readout Sim
+        input_reco, output_reco = ReadReadoutSim(GramsConfig,readout_file, output_data)
+        new = CreateReadoutTensor(GramsConfig, input_reco, output_data,GramsConfig["GenData"]["RunID"]["value"])
+    output_tensor.update(new)
+# Add to meta data how many images were generated in each run for a given batch
+    first = list(new.keys())[0]
+    meta[str(GramsConfig["GenData"]["RunID"]["value"])] = str(new[first].shape[0])
+    if (GramsConfig["GenData"]["Debug"]["value"]):
+        print(meta)
+# Save safetensor files with correct name
+    if (GramsConfig["GenData"]["MCTruth"]["value"]):
         fname = os.path.join(GramsConfig["Condor"]["OutputFolderPath"]["value"],GramsConfig["GenData"]["OutputFileBaseName"]["value"]+"_"+str(GramsConfig["GenData"]["RunID"]["value"])+".safetensors")
         save_file(output_tensor,fname, metadata=meta)
     else:
-        print("GramsDetSim data generation unimplemented... for now")
+        fname = os.path.join(GramsConfig["Condor"]["OutputFolderPath"]["value"],GramsConfig["GenData"]["OutputFileBaseName"]["value"]+"_EXP"+"_"+str(GramsConfig["GenData"]["RunID"]["value"])+".safetensors")
+        save_file(output_tensor,fname, metadata=meta)
